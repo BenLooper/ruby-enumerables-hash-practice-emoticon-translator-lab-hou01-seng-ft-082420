91b296b19defee2e35d2bc1cba1997024be95b57
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-22 22:25:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,7 @@ require 'pry'
 def load_library(path_file)
   library = YAML.load_file(path_file) 
   results = library.each_with_object({}) do |(emo,emo_array) , emo_library|
-    emo_library[emo] = ''
-    emo_library[emo] = ''
+    emo_library[emo] = {english: emo_array[0],japanese: emo_array[1]} 
     binding.pry 
   end 
   
