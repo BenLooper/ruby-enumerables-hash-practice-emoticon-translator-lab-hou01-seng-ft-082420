b0f407aeb402a08248496c7ce539c850d9e1c3b8
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-22 23:36:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(path_file,given_emote)
     index = emote_list.find_index(emo)
     if emote_list[index][:english] == given_emote
       binding.pry
-        emote_list[index][:japanese]
+        answer = emote_list[index][:japanese]
     end 
   end 
 end
