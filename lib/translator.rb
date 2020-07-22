aba9d6a8ccecac270141cda41704951ada25d155
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-22 22:26:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,8 @@ def load_library(path_file)
   library = YAML.load_file(path_file) 
   results = library.each_with_object({}) do |(emo,emo_array) , emo_library|
     emo_library[emo] = {english: emo_array[0],japanese: emo_array[1]} 
-    binding.pry 
   end 
-  
+  results
 end
 
 def get_japanese_emoticon
