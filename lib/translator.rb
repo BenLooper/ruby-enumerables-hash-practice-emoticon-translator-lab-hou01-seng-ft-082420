f290ba7b61c90a93539543aa4627a081b5964015
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-22 23:43:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,6 @@ def get_japanese_emoticon(path_file,given_emote)
   p "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path_file, given_emote)
+  
 end
